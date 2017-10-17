cdc31487ab810c33a533847492e0666d925e15af
MikeWise2718 <mwise@microsoft.com>
2017-10-17 23:15:40 +0200

    cleaned up usings

--- Assets/_scripts/StatusCtrl.cs
+++ Assets/_scripts/StatusCtrl.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using GraphAlgos;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Versioning;
-
 
 namespace BirdRouter
 {
